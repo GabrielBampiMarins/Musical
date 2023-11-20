51b7782cf78f1a0e25f338e71061f0d719a64602
Gabriel B. Marins <108159315+GabrielBampiMarins@users.noreply.github.com>
2023-11-20 09:31:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-const listadeTeclas = document.querySelectorAll(".tecla");
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
-while(contador < listadeTeclas.length ){
-    const efeito =listadeTeclas[contador].classList[1];
-    const idAudio = "som_"+efeito;
-    listadeTeclas[contador].onclick = function (){
+while(contador < listaDeTeclas.length){
+    const efeito =listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
         tocaSom(idAudio)
     }
     contador = contador + 1;
